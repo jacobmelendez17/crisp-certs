9a7172595cac8efdf4005ba1d3e5eb593b090650
jacobmelendez17 <nerdalert46@gmail.com>
2025-07-17 17:11:47 -0700

    Added Clerk authentication

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
 export const Header = () => {
-	return <header className="h-20 w-full border-b-2 border-slate-200 px-4">Header</header>;
+	return (
+		<header className="h-20 w-full border-b-2 border-slate-200 px-4">
+			<div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
+				<div className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
+					<Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+					<h1 className="tracking wide text-2xl font-extrabold text-green-600">Crisp Lingo</h1>
+				</div>
+			</div>
+		</header>
+	);
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 import './globals.css';
+import {
+	ClerkProvider,
+	SignInButton,
+	SignUpButton,
+	SignedIn,
+	SignedOut,
+	UserButton
+} from '@clerk/nextjs';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -15,8 +23,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={font.className}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={font.className}>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
