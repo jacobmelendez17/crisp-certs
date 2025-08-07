95eaf2e6f01b8f3dd7074e6fd34ffcc31fde2b3b
jacobmelendez17 <nerdalert46@gmail.com>
2025-08-07 00:08:03 -1000

    Added lesson button to learn page

---
+++ app/(main)/learn/lesson-button.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+type Props = {
+	id: number;
+	index: number;
+	totalCount: number;
+	locked?: boolean;
+	current?: boolean;
+	percentage: number;
+};
+
+export const LessonButton = ({ id, index, totalCount, locked, current, percentage }: Props) => {
+	return (
+		<div>
+			<div>{id}</div>
+		</div>
+	);
+};
