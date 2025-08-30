9075ef245a8416f75263473f2c210eeae67f3c97
jacobmelendez17 <nerdalert46@gmail.com>
2025-08-29 23:33:13 -0700

    Added exit modal card

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -13,12 +13,12 @@ function Progress({
 	return (
 		<ProgressPrimitive.Root
 			data-slot="progress"
-			className={cn('relative h-2 w-full overflow-hidden rounded-full bg-green-500/20', className)}
+			className={cn('bg-secondary relative h-2 w-full overflow-hidden rounded-full', className)}
 			{...props}
 		>
 			<ProgressPrimitive.Indicator
 				data-slot="progress-indicator"
-				className="bg-primary h-full w-full flex-1 transition-all"
+				className="h-full w-full flex-1 bg-green-500 transition-all"
 				style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
 			/>
 		</ProgressPrimitive.Root>
