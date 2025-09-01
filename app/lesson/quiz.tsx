b6799d7455e656232543e871e51d38da7c05c700
jacobmelendez17 <nerdalert46@gmail.com>
2025-08-31 23:22:44 -0700

    Added question bubble component and added question title to quiz

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { challenge_options, challenges } from '@/db/schema';
 import { Header } from './header';
+import { QuestionBubble } from './question-bubble';
 
 type Props = {
 	initialPercentage: number;
@@ -24,6 +25,15 @@ export const Quiz = ({
 }: Props) => {
 	const [hearts, setHearts] = useState(initialHearts);
 	const [percentage, setPercentage] = useState(initialPercentage);
+	const [challenges] = useState(initialLessonChallenges);
+	const [activeIndex, setActiveIndex] = useState(() => {
+		const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+		return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+	});
+
+	const challenge = challenges[activeIndex];
+
+	const title = challenge.type === 'ASSIST' ? 'Select the correct meaning' : challenge.question;
 
 	return (
 		<>
@@ -32,6 +42,18 @@ export const Quiz = ({
 				percentage={percentage}
 				hasActiveSubscription={!!userSubscription?.isActive}
 			/>
+			<div className="flex-1">
+				<div className="flex h-full items-center justify-center">
+					<div className="flex w-full flex-col gap-y-12 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0">
+						<h1 className="text-center text-lg font-bold text-neutral-700 lg:text-start lg:text-3xl">
+							{title}
+						</h1>
+						<div>
+							{challenge.type === 'SELECT' && <QuestionBubble question={challenge.question} />}
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
