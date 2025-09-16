77c5dc3c0074ae811facf2c40ede0c23b7bf8e0f
jacobmelendez17 <nerdalert46@gmail.com>
2025-09-15 23:51:03 -0700

    Added routes file for Stripe

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,50 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { stripe } from "@/lib/stripe";
+import { NextResponse } from "next/server";
+import db from "@/db/drizzle";
+import { userSubscription } from "@/db/schema";
+
+export async function POST(req: Request) {
+    const body = await req.text();
+    const signature = (await headers()).get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!,
+        );
+    } catch(error: any) {
+        return new NextResponse(`Webhook error: ${error.message}`, {
+            status: 400,
+        });
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+
+
+    if (event.type === "checkout.session.completed") {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        );
+
+        if(!session?.metadata?.userId) {
+            return new NextResponse("User ID is required", { status: 400});
+        }
+
+        await db.insert(userSubscription).values({
+            userId: session.metadata.userId,
+            stripeSubscriptionId: subscription.id,
+            stripeCustomerId: subscription.customer as string,
+            stripePriceId: subscription.items.data[0].price.id,
+            stripeCurrentPeriodEnd: new Date(
+                subscription.current_period_end * 1000,
+            )
+        })
+    }
+
+    return new NextResponse(null, { status: 200});
+}
