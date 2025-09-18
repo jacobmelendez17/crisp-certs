ee30a9fb5b451fc21951850898ee5e25c694f773
jacobmelendez17 <nerdalert46@gmail.com>
2025-09-17 23:42:40 -0700

    Added admin lesson and unit creation

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -2,9 +2,18 @@
 
 import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
+
 import { CourseList } from './course/list';
 import { CourseCreate } from './course/create';
 import { CourseEdit } from './course/edit';
+
+import { UnitList } from './unit/list';
+import { UnitCreate } from './unit/create';
+import { UnitEdit } from './unit/edit';
+
+import { LessonList } from './lesson/list';
+import { LessonCreate } from './lesson/create';
+import { LessonEdit } from './lesson/edit';
 
 const dataProvider = simpleRestProvider('/api');
 
@@ -17,6 +26,20 @@ const App = () => {
 				edit={CourseEdit}
 				recordRepresentation="title"
 				list={CourseList}
+			/>
+			<Resource
+				name="units"
+				list={UnitList}
+				create={UnitCreate}
+				edit={UnitEdit}
+				recordRepresentation="title"
+			/>
+			<Resource
+				name="lessons"
+				list={LessonList}
+				create={LessonCreate}
+				edit={LessonEdit}
+				recordRepresentation="title"
 			/>
 		</Admin>
 	);
